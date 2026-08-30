a8d4ea76b1b78b38edf82b437faa67f448382d70
dmalcev085-prog <dmalcev085@gmail.com>
2026-08-30 22:20:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 BOT_TOKEN = "8666795532:AAGKsQuB57XiBqS6tHNEVbR63y0jv_clAsY"
-ADMIN_ID = 871664936
+ADMIN_ID = 871664936  # Твій Telegram ID
 
 COMPANY_NAME = "Aroma Coffee ☕"
 COMPANY_ADDRESS = "м. Київ, вул. Велика Васильківська, 10"
 WORKING_HOURS = "Щодня: 08:00 - 21:00"
 ADMIN_PHONE = "+380501234567"
 
-SERVICES = {
-    "select_service_1": ["Капучино (300 мл)", 75],
-    "select_service_2": ["Лате (400 мл)", 90],
-    "select_service_3": ["Флет Вайт", 85],
-    "select_service_4": ["Круасан з шоколадом", 65]
+# Початкове меню (адмін зможе додавати нові прямо через бот)
+INITIAL_SERVICES = {
+    "Капучино (300 мл)": 75,
+    "Лате (400 мл)": 90,
+    "Флет Вайт": 85,
+    "Круасан з шоколадом": 65
 }
