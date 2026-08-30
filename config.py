fd885fe372eaa4c9ade483879455c1e2b4c91d1b
dmalcev085-prog <dmalcev085@gmail.com>
2026-08-30 20:02:38 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+BOT_TOKEN = "ВСТАВ_СЮДІ_ТОКЕН_БОТА"
+ADMIN_ID = 871664936
+
+COMPANY_NAME = "Aroma Coffee ☕"
+COMPANY_ADDRESS = "м. Київ, вул. Велика Васильківська, 10"
+WORKING_HOURS = "Щодня: 08:00 - 21:00"
+ADMIN_PHONE = "+380501234567"
+
+SERVICES = {
+    "select_service_1": ["Капучино (300 мл)", 75],
+    "select_service_2": ["Лате (400 мл)", 90],
+    "select_service_3": ["Флет Вайт", 85],
+    "select_service_4": ["Круасан з шоколадом", 65]
+}
